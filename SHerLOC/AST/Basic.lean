534949c24e467ff71c46437518712a9e19ed178c
John Tristan <jean.baptiste.tristan@gmail.com>
2024-08-20 18:09:03 -0400

    Moved Lean files

---
+++ SHerLOC/AST/Basic.lean
@@ -0,0 +1,11 @@
+/-
+Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jean-Baptiste Tristan
+-/
+import SHerLOC.AST.Types
+import SHerLOC.AST.Identifiers
+import SHerLOC.AST.Constants
+import SHerLOC.AST.Operations
+import SHerLOC.AST.Functions
+import SHerLOC.AST.Programs
