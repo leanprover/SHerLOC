abf158ada9f201404ffb9479d2059e7296de05f0
John Tristan <jean.baptiste.tristan@gmail.com>
2024-09-02 14:53:24 -0400

    Mode to run on single file

--- Main.lean
+++ Main.lean
@@ -7,22 +7,34 @@ import SHerLOC
 
 open System IO FilePath Process FS Std
 
-def main : IO Unit := do
-  let o ← output { cmd := "ls", args := #["Tests"] }
-  let files := o.stdout.splitOn "\n"
-  let files := files.filter fun s => s.takeRight 5 = ".mlir"
-  let mut success : Bool := true
-  let mut count : Nat := 0
-  for file in files do
-    IO.println s!"Reading {file}"
-    let fp : FilePath := System.mkFilePath ["Tests", file]
-    let content ← readFile fp
-    let content := StableHLO.parse content.data
-    match content with
-    | .ok _ =>
-      IO.println s!"Parsing {file}: success"
-    | .error e =>
-      IO.println s!"Parsing {file}: failure {e}"
-      count := count + 1
-      success := false
-  if ! success then panic! s!"{count} out of {files.length} tests failed"
+def main (args : List String) : IO Unit := do
+  if args.length = 0 then
+    let o ← output { cmd := "ls", args := #["Tests"] }
+    let files := o.stdout.splitOn "\n"
+    let files := files.filter fun s => s.takeRight 5 = ".mlir"
+    let mut success : Bool := true
+    let mut count : Nat := 0
+    for file in files do
+      IO.println s!"Reading {file}"
+      let fp : FilePath := System.mkFilePath ["Tests", file]
+      let content ← readFile fp
+      let content := StableHLO.parse content.data
+      match content with
+      | .ok _ =>
+        IO.println s!"Parsing {file}: success"
+      | .error e =>
+        IO.println s!"Parsing {file}: failure {e}"
+        count := count + 1
+        success := false
+    if ! success then panic! s!"{count} out of {files.length} tests failed"
+  else if args.length = 1 then
+    if let some _ := args[0]!.toNat? then
+      let file : String := "test" ++ args[0]! ++ ".mlir"
+      let fp : FilePath := System.mkFilePath ["Tests", file]
+      let content ← readFile fp
+      let content := StableHLO.parse content.data
+      match content with
+      | .ok p => IO.println s!"{repr p}"
+      | .error e => IO.println s!"{e}"
+    else panic! s!"Unexpected argument {args[0]!}, expected natural number"
+  else panic! s!"Unexpected number of arguments: {args.length}"
